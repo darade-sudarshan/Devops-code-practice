3f5dd625c50ffa655ac288c1c5ec56839d173718
darade.sudarshan <darade.sudarshan@gmail.com>
2025-07-18 13:45:41 +0530

    added ansible code

---
+++ ansible-code-practice/task7/sample.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+# This script is sample script to create a file in tmp dir
+
+touch sample1.txt
+echo "This is a sample file created by script" > sample1.txt
+echo "File created successfully in tmp directory"
